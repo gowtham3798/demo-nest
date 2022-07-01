082280e10b4ad6cb2c8648e4aaa944660be5c0cc
gowtham <gowtham3798@gmail.com>
2022-07-01 15:52:06 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
-  // app.enableCors();
   
   const config = new DocumentBuilder()
   .setTitle('NEST API')
